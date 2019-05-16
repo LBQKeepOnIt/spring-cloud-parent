dc73122baa324f133465bd8d1ee12c3f5fb04c69
liboqing <libq@shidegroup.com>
2019-05-16 14:41:34 +0800

    去掉无用引用

--- lbq-order-service/order-service/src/main/java/cn/com/lbq/order/service/service/OrderService.java
+++ lbq-order-service/order-service/src/main/java/cn/com/lbq/order/service/service/OrderService.java
@@ -4,7 +4,6 @@ import cn.com.lbq.order.api.entity.Order;
 import cn.com.lbq.order.service.mapper.OrderMapper;
 import cn.com.lbq.ucenter.api.client.UserClient;
 import cn.com.lbq.ucenter.api.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
